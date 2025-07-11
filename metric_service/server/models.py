86620e6ba8a171a89b0ff687d07a564f14fb73ca
Iasmina <iasmina.andreea202@gmail.com>
2025-07-11 11:50:25 +0000

    Added database scheme models in server

---
+++ metric_service/server/models.py
@@ -0,0 +1,22 @@
+import sqlite3
+import storage
+
+def create_db():
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS api_data (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            cpu INTEGER,
+            memory_left REAL,
+            used_ram REAL,
+            virtual_memory INTEGER
+        )
+    ''')
+
+    connection.commit()
+    connection.close()
+
+create_db()
+storage.call_api()
--- metric_service/server/storage.py
+++ metric_service/server/storage.py
@@ -1,34 +1,24 @@
 import sqlite3
-import json
 import requests
 
-response_API = requests.get('http://127.0.0.1:8001/show')
-data = response_API.json()
+def call_api():
+    response_API = requests.get('http://127.0.0.1:8001/show')
+    data = response_API.json()
 
-connection = sqlite3.connect("database.db")
-cursor = connection.cursor()
-
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS api_data (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        cpu INTEGER,
-        memory_left REAL,
-        used_ram REAL,
-        virtual_memory INTEGER
-    )
-''')
-
-for _ in range(10):
-    for item in data["Data"]:
-        cursor.execute('''
-            INSERT INTO api_data (cpu, memory_left, used_ram, virtual_memory)
-            VALUES (?, ?, ?, ?)
-    ''', (
-        item.get("CPU"),
-        item.get("Memory Left"),
-        item.get("Used RAM"),
-        item.get("Virtual Memory")
-    ))
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    
+    for _ in range(10):
+        for item in data["Data"]:
+            cursor.execute('''
+                INSERT INTO api_data (cpu, memory_left, used_ram, virtual_memory)
+                VALUES (?, ?, ?, ?)
+        ''', (
+            item.get("CPU"),
+            item.get("Memory Left"),
+            item.get("Used RAM"),
+            item.get("Virtual Memory")
+        ))
         
-connection.commit()
-connection.close()
+    connection.commit()
+    connection.close()
